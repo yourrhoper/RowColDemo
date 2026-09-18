024768b07cf548cfec0f63fedb7a84bd299b30a7
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-18 22:04:27 +0700

    Объединение компонентов Row и Column

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -42,10 +42,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Column(modifier) {
-        TextCell("1")
-        TextCell("2")
-        TextCell("3")
+        Row {
+            Column {
+                TextCell("1")
+                TextCell("2")
+                TextCell("3")
+            }
+            Column {
+                TextCell("4")
+                TextCell("5")
+                TextCell("6")
+            }
+            Column {
+                TextCell("7")
+                TextCell("8")
+            }
+        }
+        Row {
+            TextCell("9")
+            TextCell("10")
+            TextCell("11")
+        }
     }
+
 }
 
 @Preview(showBackground = true)

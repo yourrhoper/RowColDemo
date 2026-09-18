92b3fec1f09841a960f351a1179c48491f3d5aa1
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-18 22:05:43 +0700

    Row(modifier.size(width = 400.dp, height = 200.dp))

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -41,30 +41,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Row {
-            Column {
-                TextCell("1")
-                TextCell("2")
-                TextCell("3")
-            }
-            Column {
-                TextCell("4")
-                TextCell("5")
-                TextCell("6")
-            }
-            Column {
-                TextCell("7")
-                TextCell("8")
-            }
-        }
-        Row {
-            TextCell("9")
-            TextCell("10")
-            TextCell("11")
-        }
+    Row(modifier.size(width = 400.dp, height = 200.dp)) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
     }
-
 }
 
 @Preview(showBackground = true)

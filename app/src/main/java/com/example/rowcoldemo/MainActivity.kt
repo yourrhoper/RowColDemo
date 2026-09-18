006f9f0431a7fc3fa826ca349e90336ac857cbca
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-18 22:17:52 +0700

    Column(horizontalAlignment = Alignment.End

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,8 +42,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.size(width = 400.dp, height = 200.dp)) {
+    Column(horizontalAlignment = Alignment.End,
+        modifier = modifier.width(250.dp)) {
         TextCell("1")
         TextCell("2")
         TextCell("3")

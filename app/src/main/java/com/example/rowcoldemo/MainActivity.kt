ed5894a2fa23379c3b840a22fd0468e7bd7acacf
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-18 22:36:22 +0700

    Вес модификатора области видимости

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -48,19 +48,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Row {
-        Text(
-            text = "Large Text\n\nMore Text",
-            Modifier.alignBy(FirstBaseline),
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = "Small Text",
-            modifier = Modifier.paddingFrom(
-                alignmentLine = FirstBaseline, before = 80.dp, after = 0.dp),
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+        TextCell("1", Modifier.weight(weight = 0.2f, fill = true))
+        TextCell("2", Modifier.weight(weight = 0.4f, fill = true))
+        TextCell("3", Modifier.weight(weight = 0.3f, fill = true))
     }
 }
 
